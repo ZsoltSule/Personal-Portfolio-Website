170c1e6c83280b3bcaf646400acae6a11783ef16
Zsolt Sule <sule_zs@yahoo.com>
2025-10-17 22:59:27 +0300

    Added some more styling with tailwind CSS and added a couple functionalities to the website

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,7 +3,13 @@ import profilePic from './assets/pictures/card-profile-pic.jpeg'
 function Card(){
     return(
         <div className="card">
-            <img src={profilePic} alt="profile picture"></img>
+            <img className="card-image" src={profilePic} alt="profile picture" ></img>
+            <h2 className="font-bold text-[40px] font-serif">Zsolt Sule</h2>
+            <p className="font-semibold italic text-[28px]">Junior Developer</p>
+            <a href="https://www.linkedin.com/in/sule-zsolt-5a3bb1357" target="_blank" rel="noopener noreferrer" className="inline-block transform transition duration-300 hover:scale-125">
+                <i className="fa-brands fa-square-linkedin text-5xl text-blue-800 hover:text-blue-900 transition-colors duration-300"></i>
+            </a>
+            
         </div>
     );
 }
