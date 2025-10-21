aab7d494530654de7e486632b87b5c443545921f
Zsolt Sule <sule_zs@yahoo.com>
2025-10-21 16:08:50 +0300

    Finished the concept and styling the home page, need to add the actual text to it

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,29 @@ function Home(){
         <>
             <div className="home-container">
                 <h1 className="title">About Me</h1>
+                <hr></hr>
                 <h2 className="subtitle">22 / Junior Developer / Oradea</h2>
-                <p className="description">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio eum hic numquam 
-                    deleniti ducimus architecto, fugiat velit exercitationem, 
-                    quas explicabo perferendis cupiditate obcaecati, minima dolores cum nisi. Veniam, iure quia?              
+                <hr></hr>
+                <p class="skills">
+                    <span data-lang="C/C++">C/C++</span>
+                    <span data-lang="JavaScript">JavaScript</span>
+                    <span data-lang="C#">C#</span>                 
+                    <span data-lang="Python">Python</span>               
                 </p>
+                <hr></hr>
+                <div className="description">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio eum hic numquam 
+                        deleniti ducimus architecto, fugiat velit exercitationem, 
+                        quas explicabo perferendis cupiditate obcaecati, minima dolores cum nisi. Veniam, iure quia?              
+                    </p>
+                    <hr></hr>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio eum hic numquam 
+                        deleniti ducimus architecto, fugiat velit exercitationem, 
+                        quas explicabo perferendis cupiditate obcaecati, minima dolores cum nisi. Veniam, iure quia?              
+                    </p>
+                </div>
             </div>
         </>
     );
