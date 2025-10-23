9e766f45bf64b32d265faf024e1c5a5860a44e3a
Zsolt Sule <sule_zs@yahoo.com>
2025-10-23 15:19:31 +0300

    Shaping the Projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,6 +7,16 @@ function Projects(){
                 <h1 className="title">My Projects</h1>
                 <hr></hr>
             </div>
+            <div className="projects-grid">
+                <img/>
+                <h2 className="project-title">Personal Website</h2>
+            </div>
+            <div className="project-tech">
+
+            </div>
+            <div className="project-links">
+                
+            </div>
         </>
     );
 }
