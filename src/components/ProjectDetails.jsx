6d9a0c1b5352a486aae2fa91b1c5843e225437c5
Zsolt Sule <sule_zs@yahoo.com>
2025-10-26 13:45:37 +0200

    Getting close to the final design to the ProjectsDetailes page

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -12,23 +12,26 @@ function ProjectDetails(){
 
     return(
         <div className="projectDetails-container p-10">
-            <Link to="/projects" className="text-yellow-300 underline hover:text-yellow-400">Back to Projects</Link>
             <div className="mt-6"> 
                 <h1 className="text-4xl font-bold text-white mb-4 text-center">{project.title}</h1>
-                <img src={project.image} alt={project.title} className="w-full h-96 object-cover rounded-lg mb-6"/>
-                <p className="text-lg mb-4">{project.longDesc}</p>
+                <img src={project.demo} alt={project.title} className="project-image w-full h-96 object-cover rounded-lg mb-6"/>
 
-                <h2 className="text-2xl text-yellow-300 mt-4 mb-2">Technologies:</h2>
-                <ul className="list-disc list-inside mb-6">
+                <h2 className="text-3xl font-bold  text-yellow-300 mt-4 mb-2 text-center">Technologies</h2>
+                <hr></hr>
+                <ul className="text-2xl font-bold list-disc list-inside mb-6 text-center mt-4">
                     {project.technologies.map((tech, i) => (<li key={i}>{tech}</li>))}
                 </ul>
-                <div className="flex gap-6">
-                    <a href={project.github} target="_blank" rel="noopener noreferrer" className="border border-yellow-300 px-4 py-2 rounded hover:bg-yellow-300 hover:text-black transition">GitHub</a>
+                <hr></hr>
+                <p className="text-lg mb-4 mt-4">{project.longDesc}</p>
+
+                <div className="flex gap-6 justify-center">
+                    <a href={project.github} target="_blank" rel="noopener noreferrer" className="inline-block bg-yellow-300 text-black font-semibold px-4 py-2 rounded hover:bg-yellow-400 transform transition duration-300 hover:scale-110 mt-4">GitHub</a>
                     {project.live && (
-                        <a href={project.live} target="_blank" rel="noopener noreferrer" className="border border-yellow-300 px-4 py-2 rounded hover:bg-yellow-300 hover:text-black transition">Live</a>
+                        <a href={project.live} target="_blank" rel="noopener noreferrer" className="inline-block bg-yellow-300 text-black font-semibold px-4 py-2 rounded hover:bg-yellow-400 transform transition duration-300 hover:scale-110 mt-4">Live</a>
                     )}
                 </div>
             </div>
+                <Link to="/projects" className="inline-block bg-yellow-300 text-black font-semibold px-4 py-2 rounded hover:bg-yellow-400 transform transition duration-300 hover:scale-110 mb-4">Back to Projects</Link>
         </div>
     );
 }
--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,11 +1,13 @@
-import project1 from '../assets/banners/banner01.png'
+import project01 from '../assets/appPics/gameplay.png'
+import project11 from '../assets/banners/banner01.png'
 
 export const projectData = [
     {
         id: "grims-adventure",
         title: "Grims's Adventure",
         longDesc: "It's game...",
-        image: project1,
+        image: project11,
+        demo: project01,
         languages: ["Python"],
         technologies: ["Pygame"],
         github: "https://github.com/ZsoltSule/Grims-Adventure",
