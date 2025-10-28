765aab57e636204927609b820957a63cea0d4fff
Zsolt Sule <sule_zs@yahoo.com>
2025-10-28 22:23:08 +0200

    Finished the Skills section on my Resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -14,6 +14,49 @@ function Resume(){
                 <hr></hr>
                 <h1 className="title">Skills</h1>
                 <hr></hr>
+                <div className="grid grid-cols-2 gap-8 mb-4 mt-4 p-2 text-xl font-semibold text-center">
+                    <div className="flex flex-col items-center">
+                        <span className="block text-2xl mb-3">Front-End</span>
+                        <div className="flex gap-1">
+                            <i className="fa-solid fa-star text-yellow-300"></i>
+                            <i className="fa-solid fa-star text-yellow-300"></i>
+                            <i className="fa-solid fa-star text-yellow-300"></i>
+                            <i className="fa-solid fa-star text-yellow-300"></i>
+                            <i className="fa-solid fa-star"></i>
+                        </div>   
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <span className="block text-2xl mb-3">Back-End</span>
+                        <div className="flex gap-1">
+                            <i className="fa-solid fa-star text-yellow-300"></i>
+                            <i className="fa-solid fa-star text-yellow-300"></i>
+                            <i className="fa-solid fa-star text-yellow-300"></i>
+                            <i className="fa-solid fa-star"></i>
+                            <i className="fa-solid fa-star"></i>
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <span className="block text-2xl mb-3">Problem Solving</span>
+                        <div className="flex gap-1">
+                            <i className="fa-solid fa-star text-yellow-300"></i>
+                            <i className="fa-solid fa-star text-yellow-300"></i>
+                            <i className="fa-solid fa-star text-yellow-300"></i>
+                            <i className="fa-solid fa-star text-yellow-300"></i>
+                            <i className="fa-solid fa-star"></i>
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-center mb-3">
+                        <span className="block text-2xl mb-3">Version Control</span>
+                        <div className="flex gap-1">
+                            <i className="fa-solid fa-star text-yellow-300"></i>
+                            <i className="fa-solid fa-star text-yellow-300"></i>
+                            <i className="fa-solid fa-star text-yellow-300"></i>
+                            <i className="fa-solid fa-star"></i>
+                            <i className="fa-solid fa-star"></i>
+                        </div>
+                    </div>       
+                </div>
+                <hr></hr>
                 <h1 className="title">Experience</h1>
                 <hr></hr>
                 <p>
