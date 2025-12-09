61e2abbff7c6367f63a615fdd3e2a5e0cdfc9f64
Zsolt Sule <sule_zs@yahoo.com>
2025-12-09 20:37:58 +0200

    Started filling the website with actual content, finished completly the home page

--- src/Card.jsx
+++ src/Card.jsx
@@ -43,7 +43,7 @@ function Card() {
                 <div className="card">
                     <img className="profilePic" src={profilePic} alt="profile picture" />
                     <h2>Zsolt Sule</h2>
-                    <p>Web Developer</p>
+                    <p>Frontend Engineer</p>
 
                     <div className="socialsIcons">
                         <a href="https://github.com/ZsoltSule" target="_blank" rel="noopener noreferrer">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
                 <div className="inner-wrapper">
                     <h1>About Me</h1>
                     <hr/>
-                    <h2>22 / Web Developer / Oradea</h2>
+                    <h2>22 / Frontend Engineer / Oradea</h2>
                     <hr/>
 
                     <div className="skills">
@@ -25,29 +25,31 @@ function Home() {
 
                     <div className="description">
                         <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio eum hic numquam 
-                            deleniti ducimus architecto, fugiat velit exercitationem, 
-                            quas explicabo perferendis cupiditate obcaecati, minima dolores cum nisi. Veniam, iure quia?
+                            Hello, I'm Zsolt, a 22-year-old Junior Frontend Engineer with a bachelor's degree in Computer Science.
+                            I've had a strong passion for technology and programming from a young age. It all began with gaming as a kid, 
+                            and later in school I was introduced to programming that’s when my journey truly started.
                         </p>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio eum hic numquam 
-                            deleniti ducimus architecto, fugiat velit exercitationem, 
-                            quas explicabo perferendis cupiditate obcaecati, minima dolores cum nisi. Veniam, iure quia?
+                            My first programming language was C++ during school, and it helped me develop my way of thinking, improve problem-solving skills, 
+                            and learn algorithms and low-level technologies.  
                         </p>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio eum hic numquam 
-                            deleniti ducimus architecto, fugiat velit exercitationem, 
-                            quas explicabo perferendis cupiditate obcaecati, minima dolores cum nisi. Veniam, iure quia?
+                            During university, I began expanding my knowledge by first learning C#. 
+                            I then continued developing my skills with Python, and later discovered my passion for frontend development. 
+                            Since then, I have focused on strengthening my frontend abilities, working with technologies such as React, TypeScript, JavaScript, HTML, CSS, and TailwindCSS,
+                            the tools I use most frequently and have the most experience with. I have also explored some backend technologies, including PHP and MySQL, to gain the ability to develop full-stack websites.
                         </p>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio eum hic numquam 
-                            deleniti ducimus architecto, fugiat velit exercitationem, 
-                            quas explicabo perferendis cupiditate obcaecati, minima dolores cum nisi. Veniam, iure quia?
+                            I have created many projects using these technologies over time. One of my biggest projects is a game built with 
+                            Python and Pygame, which took a few months to develop on my own. I created everything from scratch, including the
+                            level editor and all the graphics. Another project I’m really proud of is this website. For it, I combined several
+                            frontend technologies to build a modern, scalable, and easy-to-use platform.
                         </p>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio eum hic numquam 
-                            deleniti ducimus architecto, fugiat velit exercitationem, 
-                            quas explicabo perferendis cupiditate obcaecati, minima dolores cum nisi. Veniam, iure quia?
+                            At present, I continue developing websites and using the frontend technologies 
+                            I’ve learned to refine my skills. My goal is to improve the quality of my work, 
+                            increase my development speed, and gain even more hands-on experience. 
+                            I am fully committed to mastering frontend development.
                         </p>
                     </div>
                 </div>    
