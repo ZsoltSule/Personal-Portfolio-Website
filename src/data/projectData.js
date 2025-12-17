4dee6cad172225ddfcd6b0c01ec09973edfe255a
Zsolt Sule <sule_zs@yahoo.com>
2025-12-17 15:18:16 +0200

    Added more projects

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -4,6 +4,15 @@ import project02 from '../assets/appPics/banking.png'
 import project12 from '../assets/banners/banner02.png'
 import project03 from '../assets/appPics/django.png'
 import project13 from '../assets/banners/banner03.png'
+import project04 from '../assets/appPics/website.png'
+import project14 from '../assets/banners/banner04.png'
+import project05 from '../assets/appPics/rps.png'
+import project15 from '../assets/banners/banner05.png'
+import project06 from '../assets/appPics/pong.png'
+import project16 from '../assets/banners/banner06.png'
+import project07 from '../assets/appPics/inventory.png'
+import project17 from '../assets/banners/banner07.png'
+
 
 export const projectData = [
     {
@@ -29,6 +38,50 @@ export const projectData = [
         live: null,
     },
     {
+        id: "personal-website",
+        title: "My Website",
+        longDesc: "It's this website that you are looking at :), it features a modern, fully responsive design optimized for desktops, laptops, tablets, and mobile phones.",
+        image: project14,
+        demo: project04,
+        languages: ["JavaScript"],
+        technologies: ["React", "JavaScript", "HTML", "CSS", "TailwindCSS"],
+        github: "https://github.com/ZsoltSule/Personal-Portfolio-Website",
+        live: null,
+    },
+    {
+        id: "inventory-manager",
+        title: "Inventory Manager",
+        longDesc: "Inventory Manager is a simple web application for managing products and categories. It allows users to create, read, update, and delete products and view their associated categories. It has a nice and simle design.",
+        image: project17,
+        demo: project07,
+        languages: ["PHP"],
+        technologies: ["PHP","HTML", "CSS", "MySQL"],
+        github: "https://github.com/ZsoltSule/Inventory_Manager",
+        live: null,
+    },
+    {
+        id: "pong",
+        title: "Pong",
+        longDesc: "A fun game called Pong where you can play against another player using a split keyboard or compete against an AI.",
+        image: project16,
+        demo: project06,
+        languages: ["Python"],
+        technologies: ["Python"],
+        github: "https://github.com/ZsoltSule/PongGame",
+        live: null,
+    },
+    {
+        id: "rock-paper-scissors",
+        title: "Rock Paper Scissors",
+        longDesc: "A web application that lets you play Rock, Paper, Scissors with a friend, featuring a clean and attractive design.",
+        image: project15,
+        demo: project05,
+        languages: ["JavaScript"],
+        technologies: ["JavaScript", "HTML", "CSS"],
+        github: "https://github.com/ZsoltSule/Rock-Paper-Scissors",
+        live: null,
+    },
+    {
         id: "django-website",
         title: "Django Website",
         longDesc: "It's a website with a registration interface, and after registering, you gain access to a fully functional to-do list application.",
@@ -38,5 +91,5 @@ export const projectData = [
         technologies: ["Python", "Django", "HTML", "CSS", "Bootstrap"],
         github: "https://github.com/ZsoltSule/DjangoWebsite",
         live: null,
-    }
+    },
 ];
